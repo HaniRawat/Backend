54a470793cfa130fed3e14f18861dfd6399d0acd
HaniRawat <120402194+HaniRawat@users.noreply.github.com>
2024-08-30 21:34:47 +0530

    made routes and middleware

--- src/db/index.js
+++ src/db/index.js
@@ -7,6 +7,7 @@ const connectDB = async() => {
        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
 
        console.log(`\n MongoDB Connected !! DB HOST : ${connectionInstance.connection.host}`);
+
     } catch (error) {
         console.log("MONGODB connection error", error)
         process.exit(1)
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 // require('dotenv').config({path: './env'})
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
+import express from "express"
 
 dotenv.config({
     path: './env'
 })
+const app = express()
 
 
 
@@ -13,8 +15,7 @@ dotenv.config({
 connectDB()
 .then(() => {
     app.listen(process.env.PORT || 8000, () => {
-        console.log(`Server is running at port : $
-            {process.env.PORT}`)
+        console.log(`⚙️ Server is running at port : ${process.env.PORT}`)
     })
 })
 .catch((err) => {
